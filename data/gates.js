fd75dda82174dcc33e2023d6903a4671661c6899
William minerva <willia2minerva@gmail.com>
2025-10-03 19:57:18 +0100

    Add files via upload

---
+++ data/gates.js
@@ -0,0 +1,73 @@
+export const gates = [
+  {
+    id: "e_d",
+    name: "بوابات E-D",
+    monsters: [
+      "low_goblin",
+      "giant_spider",
+      "wild_boar"
+    ]
+  },
+  {
+    id: "b_a",
+    name: "بوابات B-A",
+    monsters: [
+      "goblin_warrior"
+    ]
+  },
+  {
+    id: "a_s",
+    name: "بوابات A-S",
+    monsters: [
+      "ghoul"
+    ]
+  },
+  {
+    id: "s_rank",
+    name: "بوابات S-Rank",
+    monsters: [
+      "phantom_knight",
+      "fire_imp",
+      "cerberus",
+      "dragon_wyrm",
+      "kamish",
+      "dark_lord"
+    ]
+  },
+  {
+    id: "double_dungeon",
+    name: "بوابات Double Dungeon",
+    monsters: [
+      "dragon_wyrm",
+      "cerberus"
+    ]
+  },
+  {
+    id: "ultimate_dungeon",
+    name: "بوابات Ultimate Dungeon",
+    monsters: [
+      "abyssal_lord"
+    ]
+  },
+  {
+    id: "a_b",
+    name: "بوابات A-B",
+    monsters: [
+      "dark_soldier"
+    ]
+  },
+  {
+    id: "c_a",
+    name: "بوابات C-A",
+    monsters: [
+      "ice_demon"
+    ]
+  },
+  {
+    id: "t_d",
+    name: "بوابة T-D (Tiers Dungeon)",
+    monsters: [
+      // أضف وحوش Tiers 1-3 إذا رغبت
+    ]
+  }
+];
