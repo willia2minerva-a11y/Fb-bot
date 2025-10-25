0455d1de4fd1bf90af54587fc222c2560fe2a20c
William minerva <willia2minerva@gmail.com>
2025-10-25 16:38:40 +0100

    Create items_gates.js

---
+++ data/items_gates.js
@@ -0,0 +1,13 @@
+// data/items_gates.js
+export const itemsGates = {
+  weak_essence: { id: 'weak_essence', name: 'جوهر ضعيف', type: 'resource', stack: 999 },
+  minor_essence: { id: 'minor_essence', name: 'جوهر بسيط', type: 'resource', stack: 999 },
+  essence_core: { id: 'essence_core', name: 'لبّ الجوهر', type: 'resource', stack: 99 },
+  supreme_essence: { id: 'supreme_essence', name: 'جوهر أسمى', type: 'resource', stack: 20 },
+  low_tier_gems: { id: 'low_tier_gems', name: 'أحجار كريمة - منخفضة', type: 'material', stack: 999 },
+  tier2_gems: { id: 'tier2_gems', name: 'أحجار كريمة - مستوى 2', type: 'material', stack: 999 },
+  rare_gems: { id: 'rare_gems', name: 'أحجار نادرة', type: 'material', stack: 99 },
+  unique_armor_part: { id: 'unique_armor_part', name: 'جزء درع فريد', type: 'material', stack: 10 },
+  dragon_blood: { id: 'dragon_blood', name: 'دم تنين', type: 'boss_drop', stack: 5 },
+  chaos_essence: { id: 'chaos_essence', name: 'جوهر الفوضى', type: 'boss_drop', stack: 5 }
+};
