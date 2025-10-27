73838d44dbade3ce470434a1f2b90005208b6d76
William minerva <willia2minerva@gmail.com>
2025-10-27 18:13:05 +0100

    Update RegistrationCommands.js

--- core/commands/RegistrationCommands.js
+++ core/commands/RegistrationCommands.js
@@ -1,5 +1,5 @@
 // core/commands/commands/RegistrationCommands.js
-import { BaseCommand } from '../BaseCommand.js';
+import { BaseCommand } from './BaseCommand.js';
 
 export class RegistrationCommands extends BaseCommand {
     getCommands() {
