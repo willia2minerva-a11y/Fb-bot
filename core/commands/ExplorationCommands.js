d0304243fb27cbb6c7bce8bf629f38bfde3cefd9
William minerva <willia2minerva@gmail.com>
2025-10-27 18:16:53 +0100

    Update ExplorationCommands.js

--- core/commands/ExplorationCommands.js
+++ core/commands/ExplorationCommands.js
@@ -1,5 +1,5 @@
 // core/commands/commands/ExplorationCommands.js
-import { BaseCommand } from '../BaseCommand.js';
+import { BaseCommand } from './BaseCommand.js';
 import { locations } from '../../../../data/locations.js';
 
 export class ExplorationCommands extends BaseCommand {
