0096e86f1a71f36dbef74c8464cb11e850b9d300
William minerva <willia2minerva@gmail.com>
2025-10-27 18:17:24 +0100

    Update GateCommands.js

--- core/commands/GateCommands.js
+++ core/commands/GateCommands.js
@@ -1,5 +1,5 @@
 // core/commands/commands/GateCommands.js
-import { BaseCommand } from '../BaseCommand.js';
+import { BaseCommand } from './BaseCommand.js';
 import { locations } from '../../../../data/locations.js';
 
 export class GateCommands extends BaseCommand {
